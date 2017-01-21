7251c4621fb60537d0ccdd632674addcfdb56a5c
GustavoGuterres <gusags@hotmail.com>
2017-01-21 09:47:20 -0200

    Criado script de movimento
    Criado um prefab do player com movimento e com sprites acompanhando o
    movimento

---
+++ Assets/Scripts/TesteMovimentacao.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TesteMovimentacao : MonoBehaviour {
+
+    public float MoveX;
+    public float MoveY;
+    public float MaxVelocity;
+    private Rigidbody2D Rb;
+    private SpriteRenderer Sr;
+    public Sprite SpriteLeft;
+    public Sprite SpriteDown;
+    public Sprite SpriteUp;
+
+
+    // Use this for initialization
+    void Start () {
+        Sr = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        MoveX = Input.GetAxis("Horizontal");
+        MoveY = Input.GetAxis("Vertical");
+        Rb = GetComponent<Rigidbody2D>();
+        Rb.velocity = new Vector2(MoveX * MaxVelocity * Time.deltaTime, MoveY * MaxVelocity * Time.deltaTime);
+
+        if (MoveX != 0) {
+            if (MoveX > 0) {
+                Sr.sprite = SpriteLeft;
+                Sr.flipX = true;
+            } else {
+                Sr.sprite = SpriteLeft;
+                Sr.flipX = false;
+            }
+        } else {
+            if (MoveY != 0) {
+                if (MoveY > 0) {
+                    Sr.sprite = SpriteUp;
+                    Sr.flipX = false;
+                } else {
+                    Sr.sprite = SpriteDown;
+                    Sr.flipX = false;
+                }
+            }
+        }
+    }
+
+}
