3d057c880319a496e9119f1b390fd31b6cc5254b
GustavoGuterres <gusags@hotmail.com>
2017-01-21 14:26:55 -0200

    muitas coisas
    Muitas mudanças: Adição de sons testes, Adição de ratos testes, Criação
    de scripts Collecting, SpawnMouse, testecollider; e mais algumas coisas

---
+++ Assets/Scripts/SpawnMouse.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnMouse : MonoBehaviour {
+     
+    public int CurrentMouses = 0;
+    private float RandomNumber;
+    public GameObject Mouse1;
+    public GameObject Mouse2;
+    public GameObject Mouse3;
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Spawn();
+	}
+
+    public void Spawn() {
+        RandomNumber = Random.Range(0f, 10f);
+        Debug.Log("deu isso: " + RandomNumber);
+    }
+}
---
+++ Assets/TesteCollisionSewer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TesteCollisionSewer : MonoBehaviour {
+
+    //public GameObject Player;
+
+	// Use this for initialization
+	void Start () {
+        //Player.GetComponent<Collecting>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision){
+        if (collision.gameObject.tag == "Sewer") {
+            Collecting.FreeMouse();
+        }
+    }
+}
