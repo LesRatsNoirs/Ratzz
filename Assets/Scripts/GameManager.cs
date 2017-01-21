28889901ba2c8db96ce0a022dc65383b08373e82
Ricardo Dias <ricardodc@gmail.com>
2017-01-21 03:06:34 -0200

    Initial Commit"

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Random = System.Random;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager instance;
+
+
+    public void Awake() {
+        this.getInstance();
+
+    }
+
+    // Singleton
+    private void getInstance() {
+        if(instance == null) {
+            instance = this;
+        } else {
+            if(instance != this) {
+                Destroy(gameObject);
+            }
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+}
