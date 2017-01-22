feb4b23253bf78431a63b65b5d0724d38f2ff676
Ricardo Dias <ricardodc@gmail.com>
2017-01-22 05:31:46 -0200

    Adding Musics

--- Assets/Scripts/BuildingComponent.cs
+++ Assets/Scripts/BuildingComponent.cs
@@ -10,7 +10,6 @@ public class BuildingComponent : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
-        Debug.Log("Build Position: " + transform.position);
 	}
 	
 	// Update is called once per frame
