1f07614ebc5811c86f0c3d16499d9db5813a6602
GustavoGuterres <gusags@hotmail.com>
2017-01-22 11:07:03 -0200

    chinelo rotacional
    chinelo roda

--- Assets/Scripts/Ammo.cs
+++ Assets/Scripts/Ammo.cs
@@ -6,6 +6,7 @@ public class Ammo : MonoBehaviour {
 
     public float AmmoVelocity =10;
     private Rigidbody2D Rb;
+    public float rot;
 
 	// Use this for initialization
 	void Start () {
@@ -16,7 +17,8 @@ public class Ammo : MonoBehaviour {
 	void Update () {
         Rb.velocity = transform.right * AmmoVelocity;
         Debug.Log(transform.forward);
-	}
+        transform.Rotate(new Vector3(0, 0, rot));
+    }
 
     private void OnCollisionEnter2D(Collision2D collision){
         Destroy(gameObject);
